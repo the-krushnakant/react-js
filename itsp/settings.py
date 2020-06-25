a663d5c1f966f315485ee41cdb674cf52b144705
the-krushnakant <krushnabhattad15@gmail.com>
2020-06-25 23:47:37 +0530

    Update settings.py

--- itsp/settings.py
+++ itsp/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*o#-htjvsza@u#&ou6n9z59tsd+kxjnnege4t))(8j0$1)w3*m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
